9c8c98dc2d03dcf96ea905634900e6cddaa62353
Istiaque Mahamud <istiaquemahamud@Istiaques-MacBook-Air.local>
2025-05-04 00:57:56 +0600

    Array Practice

--- Array/q3.c
+++ Array/q3.c
@@ -7,9 +7,15 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    for(int i=n-1;i>=0;i--){
-        printf("%d ",arr[i]);
+    int sum=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]%2==0){
+            sum+=arr[i];
+        }
     }
+    printf("%d",sum);
 
+ 
     return 0;
+
 }
