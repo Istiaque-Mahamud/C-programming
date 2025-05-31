30a99e18d43f78f3fc5b6095f4a848eda3e56325
Istiaqe-Mahamud <istiaque.mahamud@gmail.com>
2025-05-31 22:49:02 +0600

    Array Practice

---
+++ Array/q12.c
@@ -0,0 +1,41 @@
+ #include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[100]; 
+   
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int number, position;
+ 
+    printf("number: ");
+    scanf("%d", &number);
+    printf("position: ");
+    scanf("%d", &position);
+
+    if (position < 0 || position > n) {
+        printf("Invalid position");
+    } else {
+ 
+        for (int i = n; i > position; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+      
+        arr[position] = number;
+        n++; 
+    }
+
+   
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
